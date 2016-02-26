feb3cadf5cfb2a097a9d7b5329a41ad006bc0b49
IQ <iq.at.sk131@gmail.com>
2016-02-26 23:52:13 +0700

    Refresh .gitignore

--- Assets/VRMWdb.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class VRMWdb : MonoBehaviour {
-	public static IFirebase firebase = Firebase.CreateNew ("https://sweltering-heat-6741.firebaseio.com");
-}

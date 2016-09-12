5004b3a7fd8dc3149457dd209bb33e64756f62b1
IQ <iq.at.sk131@gmail.com>
2016-09-12 16:12:24 +0700

    Temp

--- Assets/VRMW Script/AnswerTrackableBehavior.cs
+++ Assets/VRMW Script/AnswerTrackableBehavior.cs
@@ -19,13 +19,11 @@ namespace Vuforia
 	public class AnswerTrackableBehavior : MonoBehaviour,
 	ITrackableEventHandler
 	{
-		[SerializeField] private GameObject YesObject;
-		[SerializeField] private GameObject NoObject;
+		[SerializeField] private string Value;
 
 		#region PRIVATE_MEMBER_VARIABLES
 
 		private TrackableBehaviour mTrackableBehaviour;
-		private bool isTrack;
 		#endregion // PRIVATE_MEMBER_VARIABLES
 
 
@@ -36,10 +34,6 @@ namespace Vuforia
 		{
 			//CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
 
-
-			//Initialize Tracking and Used state
-			isTrack = false;
-
 			//Get track component
 			mTrackableBehaviour = GetComponent<TrackableBehaviour>();
 			if (mTrackableBehaviour)
@@ -49,21 +43,6 @@ namespace Vuforia
 
 		}
 		#endregion // UNTIY_MONOBEHAVIOUR_METHODS
-
-
-		/// <summary>
-		/// Calcualte Distance from this card to Action Scanner
-		/// </summary>
-		public float calcYesDistance(){
-			Vector3 focusPos = YesObject.transform.position;
-			Vector3 thisPos = this.transform.position;
-			return Mathf.Sqrt((focusPos.x-thisPos.x)*(focusPos.x-thisPos.x) + (focusPos.y-thisPos.y)*(focusPos.y-thisPos.y)  + (focusPos.z-thisPos.z)*(focusPos.z-thisPos.z));
-		}
-		public float calcNoDistance(){
-			Vector3 focusPos = NoObject.transform.position;
-			Vector3 thisPos = this.transform.position;
-			return Mathf.Sqrt((focusPos.x-thisPos.x)*(focusPos.x-thisPos.x) + (focusPos.y-thisPos.y)*(focusPos.y-thisPos.y)  + (focusPos.z-thisPos.z)*(focusPos.z-thisPos.z));
-		}
 
 		#region PUBLIC_METHODS
 
@@ -79,19 +58,13 @@ namespace Vuforia
 				newStatus == TrackableBehaviour.Status.TRACKED ||
 				newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
 			{
-				//Change Tracking state to true
-				isTrack=true;
-				Timing.RunCoroutine(AnswerQuestion());
-
-				//Show Attack Model
+				if (VRMWdb.isInitiated){
+					VRMWdb.SetQuestionUserAnswer(Value);
+				}
 				OnTrackingFound();
 			}
 			else
 			{
-
-				//Change Tracking state to false
-				isTrack=false;
-
 				//Hide Tracking Model
 				OnTrackingLost ();
 			}
@@ -102,38 +75,6 @@ namespace Vuforia
 
 
 		#region PRIVATE_METHODS
-
-
-		private IEnumerator<float> AnswerQuestion(){
-			while(isTrack){
-				yield return Timing.WaitForSeconds(0.1f);
-
-				//Don't Start update until VRMWdb is initiated
-				if (!VRMWdb.isInitiated)
-					continue ;
-
-				//If distance to action scanner less than 700, do trigger
-				if (calcYesDistance() < 700) {
-
-					//Hide Attack Model on the card
-					OnTrackingLost ();
-					VRMWdb.SetQuestionUserAnswer("Yes");
-				} else if (calcNoDistance() < 700) {
-
-					//Hide Attack Model on the card
-					OnTrackingLost ();
-					VRMWdb.SetQuestionUserAnswer("No");
-				} else {
-					//If distance is more than 700,
-					
-					//Show Attack Model on the card
-					if(isTrack)OnTrackingFound();
-					
-				}
-
-			}
-			yield return 0f;
-		}
 
 		private void OnTrackingFound()
 		{

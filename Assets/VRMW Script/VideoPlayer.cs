1502c5fa836b1fe2f59bffa75c02271ad731c227
IQ <iq.at.sk131@gmail.com>
2016-09-25 12:54:17 +0700

    Add Video [Windows Player]

---
+++ Assets/VRMW Script/VideoPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoPlayer : MonoBehaviour {
+	public MovieTexture movTexture;
+	void Start() {
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		GetComponent<AudioSource>().clip = movTexture.audioClip;
+		movTexture.Play();
+		GetComponent<AudioSource>().Play();
+	}
+}
